4c5863a5d22346614216bf297ba3c04154be168e
Andrei Gridnev <andrew.gridnev@gmail.com>
2017-02-19 17:45:22 +1100

    Add validation for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author
+
+  validates :post, presence: true
+  validates :author, presence: true
+  validates :body, presence: true
 end
