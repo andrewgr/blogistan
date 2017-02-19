6f2b20d739b7a1ba03c205cb7dbd253e6eb2a258
Andrei Gridnev <andrew.gridnev@gmail.com>
2017-02-19 18:53:43 +1100

    Add deleted_at to Post model

---
+++ db/migrate/20170219075258_add_deleted_at_to_post.rb
@@ -0,0 +1,5 @@
+class AddDeletedAtToPost < ActiveRecord::Migration[5.0]
+  def change
+    add_column :posts, :deleted_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170219072036) do
+ActiveRecord::Schema.define(version: 20170219075258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170219072036) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.datetime "published_at"
+    t.datetime "deleted_at"
     t.index ["author_id"], name: "index_posts_on_author_id", using: :btree
   end
 
