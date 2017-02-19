6a68add018ed0dff6234a77dad97e4c2f8c1cfa2
Andrei Gridnev <andrew.gridnev@gmail.com>
2017-02-19 17:50:56 +1100

    Refactoring

--- app/models/event_sourced/post.rb
+++ app/models/event_sourced/post.rb
@@ -3,9 +3,7 @@ module EventSourced
     include Potoroo::AggregateRoot
     include Potoroo::Projection
 
-    def state
-      @state
-    end
+    attr_reader :author_id, :body, :state
 
     def create(author_id, body)
       raise(ArgumentError, 'author_id cannot be empty') if author_id.blank?
@@ -14,6 +12,10 @@ module EventSourced
       emit PostCreated, author_id: author_id, body: body
     end
 
-    apply(PostCreated) { |_| @state = :hidden }
+    apply(PostCreated) do |e|
+      @state = :hidden
+      @author_id = e.author_id
+      @body = e.body
+    end
   end
 end
